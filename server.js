1b16b03b8ec25b71f5f7e9b019d9e35fd139aaa0
canderson22 <charles.anderson.dev@gmail.com>
2018-10-13 11:58:42 -0700

    done

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 require('dotenv').config();
+// require('newrelic');
 const express = require('express');
+const logger = require('morgan');
 const path = require('path');
 const app = express();
-const proxy = require('http-proxy-middleware');
-const { routes } = require('./config.json');
+var cors = require('cors');
+app.use(cors());
+var proxy = require('express-http-proxy');
+const PORT = process.env.PORT || 3000;
+app.use(logger('dev'));
 
-app.use('/home/:id', express.static('public'));
+// REDIS IMPLEMENTATION
+var redis = require('redis');
+var client = redis.createClient(6379, '27.0.0.1', {
+  no_ready_check: true
+});
+client.on('connect', function() {
+  console.log('Redis client connected');
+});
 
-for (route of routes) {
-    app.use(route.route,
-        proxy({
-            target: route.address
-        })
-    );
-}
+app.use('/:id', express.static('public'));
+app.use('/house', (req, res, next) => {
+  req.url = '/house' + req.url;
+  next();
+});
+app.use('/house', proxy('Lb-1291171679.us-east-1.elb.amazonaws.com'));
 
-
-app.listen(process.env.PORT, () => {
-  console.log(`server running at: http://localhost:${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`server running at port: ${PORT}`);
 });
