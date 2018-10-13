6bb0b79363905ed4913733f2f39e4d8895d090eb
canderson22 <charles.anderson.dev@gmail.com>
2018-10-13 15:08:16 -0700

    new

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ const PORT = process.env.PORT || 3000;
 app.use(logger('dev'));
 
 // REDIS IMPLEMENTATION
-var redis = require('redis');
-var client = redis.createClient(6379, '27.0.0.1', {
-  no_ready_check: true
-});
-client.on('connect', function() {
-  console.log('Redis client connected');
-});
+// var redis = require('redis');
+// var client = redis.createClient(6379, '27.0.0.1', {
+//   no_ready_check: true
+// });
+// client.on('connect', function() {
+//   console.log('Redis client connected');
+// });
 
 app.use('/:id', express.static('public'));
 app.use('/house', (req, res, next) => {
